8dea1dcbb3e8923ee85ef5c1189ef980e75a5dbc
TynK-M <matteo.pauroso@gmail.com>
2026-05-29 09:55:31 +0200

    feat(run): run current python file
    Adds runPyFile function to retrieve the current file and run it in a
    split terminal with the correct python from the PATH

--- lua/serpy/init.lua
+++ lua/serpy/init.lua
@@ -1,3 +1,13 @@
 local M = {}
+local utils = require("serpy.utils")
+
+function M.runPyFile()
+	local py = utils.getPythonCmd()
+	local path = utils.getOpenFilePath()
+
+	local cmd = string.format("split | terminal %s %q", py, path)
+
+	vim.cmd(cmd)
+end
 
 return M
---
+++ lua/serpy/utils.lua
@@ -0,0 +1,19 @@
+local M = {}
+
+function M.getPythonCmd()
+	if vim.fn.executable("python3") == 1 then
+		return "python3"
+	elseif vim.fn.executable("python") == 1 then
+		return "python"
+	elseif vim.fn.executable("py") == 1 then
+		return "py"
+	else
+		return "python3"
+	end
+end
+
+function M.getOpenFilePath()
+	return vim.fn.expand("%")
+end
+
+return M
