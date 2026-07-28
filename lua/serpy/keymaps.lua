abf6e79e669f6b7e8bab0c82ca37cad5ea09a4ac
TynK-M <matteo.pauroso@gmail.com>
2026-07-28 16:35:37 +0200

    fix: nil for not provided keymap

--- lua/serpy/keymaps.lua
+++ lua/serpy/keymaps.lua
@@ -9,31 +9,41 @@ function M.setup(opts)
 	local km = opts.keymaps
 
 	-- Python
-	map("n", km.pyrun_current, function()
-		require("serpy").run("python")
-	end, { desc = "serpy(python): run current file" })
+	if km.pyrun_current ~= nil then
+		map("n", km.pyrun_current, function()
+			require("serpy").run("python")
+		end, { desc = "serpy(python): run current file" })
+	end
 
-	map("n", km.pyrun_current_with_flags, function()
-		vim.ui.input({ prompt = "Flags: " }, function(input)
-			require("serpy").run("python", input or "")
-		end)
-	end, { desc = "serpy(python): run current file with flags" })
+	if km.pyrun_current_with_flags ~= nil then
+		map("n", km.pyrun_current_with_flags, function()
+			vim.ui.input({ prompt = "Flags: " }, function(input)
+				require("serpy").run("python", input or "")
+			end)
+		end, { desc = "serpy(python): run current file with flags" })
+	end
 
-	map("n", km.pydoc, function()
-		vim.ui.input({ prompt = "Docs: " }, function(input)
-			require("serpy").docs("python", input or "")
-		end)
-	end, { desc = "serpy(python): search a term in the pydoc" })
+	if km.pydoc ~= nil then
+		map("n", km.pydoc, function()
+			vim.ui.input({ prompt = "Docs: " }, function(input)
+				require("serpy").docs("python", input or "")
+			end)
+		end, { desc = "serpy(python): search a term in the pydoc" })
+	end
 
-	map("n", km.pydoc_current_word, function()
-		local word = vim.fn.expand("<cword>")
-		require("serpy").docs("python", word)
-	end, { desc = "serpy(python): search current word in the pydoc" })
+	if km.pydoc_current_word ~= nil then
+		map("n", km.pydoc_current_word, function()
+			local word = vim.fn.expand("<cword>")
+			require("serpy").docs("python", word)
+		end, { desc = "serpy(python): search current word in the pydoc" })
+	end
 
 	-- Lua
-	map("n", km.luarun_current, function()
-		require("serpy").run("lua")
-	end, { desc = "serpy(lua): run current file" })
+	if km.luarun_current ~= nil then
+		map("n", km.luarun_current, function()
+			require("serpy").run("lua")
+		end, { desc = "serpy(lua): run current file" })
+	end
 end
 
 return M
